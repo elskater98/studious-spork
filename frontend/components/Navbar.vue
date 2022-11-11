2eb1e82386271d41135b77da44f51654fc198fa5
elskater98 <elskater98@gmail.com>
2022-11-11 20:18:39 +0100

    - Create Nuxt Project

---
+++ frontend/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-navbar fixed-top="true">
+    <template #end>
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
